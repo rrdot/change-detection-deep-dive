476a8abe2ff55cc639bccfdcb3602ce351490a92
RusselDotdot <dotdot.russel@seedtech.ph>
2024-08-07 17:04:56 +0800

    Finish change-detection-deep-dive

---
+++ src/app/messages/messages-list/messages-list.component.ts
@@ -0,0 +1,48 @@
+import {
+  ChangeDetectionStrategy,
+  ChangeDetectorRef,
+  Component,
+  DestroyRef,
+  inject,
+  input,
+  OnInit,
+} from '@angular/core';
+import { MessagesService } from '../messages.service';
+import { AsyncPipe } from '@angular/common';
+
+@Component({
+  selector: 'app-messages-list',
+  standalone: true,
+  // imports: [AsyncPipe],
+  templateUrl: './messages-list.component.html',
+  styleUrl: './messages-list.component.css',
+  changeDetection: ChangeDetectionStrategy.OnPush,
+})
+export class MessagesListComponent {
+  private messagesService = inject(MessagesService);
+  // private cdRef = inject(ChangeDetectorRef);
+  // private destroyRef = inject(DestroyRef);
+
+  // messages: string[] = [];
+
+  // ngOnInit(): void {
+  //   const subscription = this.messagesService.messages$.subscribe(
+  //     (messages) => {
+  //       this.messages = messages;
+  //       this.cdRef.markForCheck();
+  //     }
+  //   );
+
+  //   this.destroyRef.onDestroy(() => {
+  //     subscription.unsubscribe();
+  //   });
+  // }
+  // messages$ = this.messagesService.messages$;
+
+  messages = this.messagesService.allMessages;
+
+  get debugOutput() {
+    console.log('[MessagesList] "debugOutput" binding re-evaluated.');
+    return 'MessagesList Component Debug Output';
+  }
+}
